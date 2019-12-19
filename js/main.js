282534f68b7deb40d514050a4a18b2510e19e46f
Alexey Khodov <al.pascia@gmail.com>
2019-12-20 01:14:44 +0300

    Верхнее меню липнет на десктопе

--- js/main.js
+++ js/main.js
@@ -1,28 +1,57 @@
-$(function(){
-    $('.menuToggle').on('click', function() {
-       $('.overlay').slideToggle(300, function(){
-            if( $(this).css('display') === "none"){
-                $(this).removeAttr('style');
-            }
-       });      
-
+$(function () {
+  $('.menuToggle').on('click', function () {
+    $('.overlay').slideToggle(300, function () {
+      if ($(this).css('display') === 'none') {
+        $(this).removeAttr('style');
+      }
     });
+  });
 });
 
-$(function(){
-    $('.show_popup').on('click', function() {
-       $('.callback-form').slideToggle(300, function(){
-            if( $(this).css('display') === "none"){
-                $(this).removeAttr('style');
-            }
-            
-       });      
-    
+$(function () {
+  $('.show_popup').on('click', function () {
+    $('.callback-form').slideToggle(300, function () {
+      if ($(this).css('display') === 'none') {
+        $(this).removeAttr('style');
+      }
     });
+  });
 
-    $('.close').click(function() { 
-        $('.callback-form').hide();     
-    })
+  $('.close').click(function () {
+    $('.callback-form').hide();
+  });
 });
 
-
+$(function () {
+  var $topMenu = $('.topmenu');
+  var isTransitioning;
+  $(window).on('scroll', function (e) {
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      if (window.scrollY >= 145) {
+        if (!isTransitioning && !$topMenu.hasClass('fixed')) {
+          isTransitioning = true;
+          $topMenu
+            .hide()
+            .addClass('fixed')
+            .fadeIn({
+              complete: function () {
+                isTransitioning = false;
+              }
+            });
+        }
+      } else {
+        if (!isTransitioning && $topMenu.hasClass('fixed')) {
+          isTransitioning = true;
+          $topMenu.fadeOut({
+            complete: function () {
+              $topMenu
+                .removeClass('fixed')
+                .show();
+              isTransitioning = false;
+            }
+          });
+        }
+      }
+    }
+  });
+});
