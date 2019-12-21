a191675efbf5073f11762a579d6c4f36d799341a
Alexey Khodov <al.pascia@gmail.com>
2019-12-21 17:09:48 +0300

    Browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
-const watch = require('gulp-watch');
 
-gulp.task('sass-compile', function(){
-    return gulp.src('./scss/**/*.scss')
+require('gulp-watch');
+const browserSync = require('browser-sync').create();
+
+gulp.task('sass-compile', function () {
+  return gulp.src('./scss/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./css/'))
-})
+    .pipe(gulp.dest('./css/'));
+});
 
-gulp.task('watch', function(){
-    gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'))
-})
+gulp.task('browser-sync', function () {
+
+});
+
+gulp.task('watch', function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'));
+
+  gulp
+    .watch(['./scss/**/*.scss', './*.html', './js/**/*.js'], gulp.series('sass-compile'))
+    .on('change', browserSync.reload);
+});
